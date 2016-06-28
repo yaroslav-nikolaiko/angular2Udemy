ef0fde55b785e3bd7e64bcdfc18db325c039d91e
yaroslav <nikolaiko.yaroslav@gmail.com>
2016-06-28 21:28:54 +0300

    spotify search with observables

--- observables/app/app.component.ts
+++ observables/app/app.component.ts
@@ -1,6 +1,7 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import {Component} from 'angular2/core';
+import {Observable} from "rxjs/Rx";
 
 @Component({
     selector: 'my-app',
@@ -10,5 +11,35 @@ import {Component} from 'angular2/core';
 })
 export class AppComponent {
     constructor(){
+        //this.jqueryImplementation();
+        var keyups = Observable.fromEvent($("#search"), "keyup")
+            .map(e=>e.target.value)
+            .filter(text=>text.length >= 3)
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .flatMap(text=>{
+                var url = "https://api.spotify.com/v1/search?type=artist&q="+text;
+                var promise = $.getJSON(url);
+                return Observable.fromPromise(promise);
+            });
+        keyups.subscribe(data => console.log(data));
+    }
+
+    jqueryImplementation(){
+        var debounced = _.debounce(function (text) {
+            var url = "https://api.spotify.com/v1/search?type=artist&q="+text;
+            $.getJSON(url, function(artists){
+                console.log(artists);
+            })
+        }, 400);
+
+        $("#search").keyup(function (e) {
+            var text = e.target.value;
+
+            if(text.length < 3) return;
+
+            debounced(text);
+
+        })
     }
 }
